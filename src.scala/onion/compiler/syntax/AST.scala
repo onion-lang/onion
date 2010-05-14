7ecbe73ea4ed7c04acc8dcf18650a80b5cfcc00e
kmizu <mizukota@gmail.com>
2010-05-14 23:10:20 +0900

    Add an AST node's type for module declaration.

--- src.scala/onion/compiler/syntax/AST.scala
+++ src.scala/onion/compiler/syntax/AST.scala
@@ -1,7 +1,10 @@
 package onion.lang.syntax
 
 abstract sealed class ASTElement(val pos: Position)
-case class ASTCompilationUnit(pos: Position, sourceFile: String = "<generated>", imports: ASTImportClause, toplevels: List[ASTToplevel])
+case class ASTCompilationUnit(
+  pos: Position, sourceFile: String = "<generated>", module: ModuleDeclaration,
+  imports: ASTImportClause, toplevels: List[ASTToplevel])
+case class ASTModuleDeclaration(pos: Position, name: String)
 case class ASTImportClause(pos: Position, mapping: List[(String, String)])
 abstract sealed class ASTToplevel(pos: Position)
 case class Position(line: Int, column: Int)

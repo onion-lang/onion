c72a98eacb6da9ba76798ef03363091b61ca5814
kmizu <mizukota@gmail.com>
2010-05-14 23:07:10 +0900

    Add several types for AST.

--- src.scala/onion/compiler/syntax/AST.scala
+++ src.scala/onion/compiler/syntax/AST.scala
@@ -1,4 +1,7 @@
 package onion.lang.syntax
 
 abstract sealed class ASTElement(val pos: Position)
+case class ASTCompilationUnit(pos: Position, sourceFile: String = "<generated>", imports: ASTImportClause, toplevels: List[ASTToplevel])
+case class ASTImportClause(pos: Position, mapping: List[(String, String)])
+abstract sealed class ASTToplevel(pos: Position)
 case class Position(line: Int, column: Int)

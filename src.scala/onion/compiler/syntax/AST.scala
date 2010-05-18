1e9813eb2be68ef9313e6c6c6d6d64c023ed8bd2
kmizu <mizukota@gmail.com>
2010-05-18 16:03:30 +0900

    Add several node classes.

--- src.scala/onion/compiler/syntax/AST.scala
+++ src.scala/onion/compiler/syntax/AST.scala
@@ -1,12 +1,94 @@
 package onion.lang.syntax
 
 object AST {
+  abstract sealed class TypeDescriptor
+  case class PrimitiveType(kind: PrimitiveTypeKind) extends TypeDescriptor
+  case class ReferenceType(name: String, qualified: Boolean) extends TypeDescriptor
+  case class ParameterizedType(component: TypeDescriptor, params: List[TypeDescriptor]) extends TypeDescriptor
+  case class ArrayType(component: TypeDescriptor) extends TypeDescriptor
+  abstract sealed class PrimitiveTypeKind
+  case object KByte extends PrimitiveTypeKind
+  case object KShort extends PrimitiveTypeKind
+  case object KInt extends PrimitiveTypeKind
+  case object KLong extends PrimitiveTypeKind
+  case object KChar extends PrimitiveTypeKind
+  case object KFloat extends PrimitiveTypeKind
+  case object KDouble extends PrimitiveTypeKind
+  case object KBoolean extends PrimitiveTypeKind
+  case object KVoid extends PrimitiveTypeKind
+  case class TypeNode(pos: Position, desc: TypeDescriptor) extends Node
+  case class Position(line: Int, column: Int)
   abstract sealed class Node{ def pos: Position }
+  case class Argument(pos: Position, name: String, typeRef: TypeNode) extends Node
   case class CompilationUnit(
     pos: Position, sourceFile: String = "<generated>", module: ModuleDeclaration,
     imports: ImportClause, toplevels: List[Toplevel]) extends Node
   case class ModuleDeclaration(pos: Position, name: String) extends Node
   case class ImportClause(pos: Position, mapping: List[(String, String)]) extends Node
   abstract sealed class Toplevel(pos: Position) extends Node
-  case class Position(line: Int, column: Int)
+  abstract sealed class Expression extends Node
+  abstract sealed class BinaryExpression(symbol: String) extends Node {
+    def left: Expression
+    def right: Expression
+  }
+  abstract sealed class UnaryExpression(symbol: String) extends Node {
+    def target: Expression
+  }
+  case class Addition(pos: Position, left: Expression, right: Expression) extends BinaryExpression("+")
+  case class AdditionAssignment(pos: Position, left: Expression, right: Expression) extends BinaryExpression("+=")
+  case class Assignment(pos: Position, left: Expression, right: Expression) extends BinaryExpression("=")
+  case class BitAnd(pos: Position, left: Expression, right: Expression) extends BinaryExpression("&")
+  case class BitOr(pos: Position, left: Expression, right: Expression) extends BinaryExpression("|")
+  case class BooleanLiteral(pos: Position, value: Boolean) extends Expression
+  case class Cast(pos: Position, src: Expression, to: TypeNode) extends Expression
+  case class CharacterLiteral(pos: Position, value: Char) extends Expression
+  case class ClosureExpression(pos: Position, typeRef: TypeNode, args: Argument, returns: TypeNode) extends Expression
+  case class CurrentInstance(pos: Position) extends Expression
+  case class Division(pos: Position, left: Expression, right: Expression) extends BinaryExpression("/")
+  case class DivisionAssignment(pos: Position, left: Expression, right: Expression) extends BinaryExpression("/=")
+  case class DoubleLiteral(pos: Position, value: Double) extends Expression
+  case class Elvis(pos: Position, left: Expression, right: Expression) extends BinaryExpression(":?")
+  case class Equal(pos: Position, left: Expression, right: Expression) extends BinaryExpression("==")
+  case class FloatLiteral(pos: Position, value: Float) extends Expression
+  case class GreaterOrEqual(pos: Position, left: Expression, right: Expression) extends BinaryExpression(">=")
+  case class GreaterThan(pos: Position, left: Expression, right: Expression) extends BinaryExpression(">")
+  case class Id(pos: Position, name: String) extends Expression
+  case class Indexing(pos: Position, left: Expression, right: Expression) extends BinaryExpression("[]")
+  case class IntegerLiteral(pos: Position, value: Int) extends Expression
+  case class IsInstance(pos: Position, typeRef: TypeNode) extends Expression
+  case class LessOrEqual(pos: Position, left: Expression, right: Expression) extends BinaryExpression("<=")
+  case class LessThan(pos: Position, left: Expression, right: Expression) extends BinaryExpression("<")  
+  case class ListLiteral(pos: Position, elements: List[Expression]) extends Expression
+  case class LogicalAnd(pos: Position, left: Expression, right: Expression) extends BinaryExpression("&&")
+  case class LogicalOr(pos: Position, left: Expression, right: Expression) extends BinaryExpression("||")
+  case class LogicalRightShift(pos: Position, left: Expression, right: Expression) extends BinaryExpression(">>>")
+  case class LongLiteral(pos: Position, value: Long) extends Expression
+  case class MathLeftShift(pos: Position, left: Expression, right: Expression) extends BinaryExpression("<<")
+  case class MathRightShift(pos: Position, left: Expression, right: Expression) extends BinaryExpression(">>")
+  case class MemberSelection(pos: Position, target: Expression, name: String) extends Expression
+  case class MethodCall(pos: Position, target: Expression, name: String, args: List[Expression]) extends Expression
+  case class Modulo(pos: Position, left: Expression, right: Expression) extends BinaryExpression("%")
+  case class ModuloAssignment(pos: Position, left: Expression, right: Expression) extends BinaryExpression("%=")
+  case class Multiplication(pos: Position, left: Expression, right: Expression) extends BinaryExpression("*")
+  case class MultiplicationAssignment(pos: Position, left: Expression, right: Expression) extends BinaryExpression("*=")
+  case class Negate(pos: Position, target: Expression) extends UnaryExpression("-")
+  case class NewArray(pos: Position, typeRef: TypeNode, args: List[Expression]) extends Expression
+  case class NewObject(pos: Position, typeRef: TypeNode, args: List[Expression]) extends Expression
+  case class Not(pos: Position, target: Expression) extends UnaryExpression("!")
+  case class NotEqual(pos: Position, left: Expression, right: Expression) extends BinaryExpression("!=")
+  case class NullLiteral(pos: Position) extends Expression
+  case class Posit(pos: Position, target: Expression) extends UnaryExpression("+")
+  case class PostDecrement(pos: Position, target: Expression) extends UnaryExpression("--")
+  case class PostIncrement(pos: Position, target: Expression) extends UnaryExpression("++")
+  case class ReferenceEqual(pos: Position, target: Expression) extends UnaryExpression("===")
+  case class ReferenceNotEqual(pos: Position, target: Expression) extends UnaryExpression("!==")
+  case class UnqualifiedFieldReference(pos: Position, name: String) extends Expression
+  case class UnqualifiedMethodCall(pos: Position, name: String, args: Expression) extends Expression
+  case class StaticIDExpression(pos: Position, typeRef: TypeNode, name: String) extends Expression
+  case class StaticMethodCall(pos: Position, typeRef: TypeNode, name: String, args: List[Expression]) extends Expression
+  case class StringLiteral(pos: Position, value: String) extends Expression
+  case class Subtraction(pos: Position, left: Expression, right: Expression) extends BinaryExpression("-")
+  case class SubtractionAssignment(pos: Position, left: Expression, right: Expression) extends BinaryExpression("-=")
+  case class SuperMethodCall(pos: Position, name: String, args: List[Expression]) extends Expression
+  case class XOR(pos: Position, left: Expression, right: Expression) extends BinaryExpression("^")  
 }  

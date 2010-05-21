1dac32739a2cb427e521e7af702aa83b37dfb803
kmizu <mizukota@gmail.com>
2010-05-21 18:25:04 +0900

    Change types of sourceFile, module, imports in CompilationUnit to Option[X] because these fields maybe omitted.

--- src.scala/onion/compiler/syntax/AST.scala
+++ src.scala/onion/compiler/syntax/AST.scala
@@ -35,8 +35,8 @@ object AST {
   abstract sealed class Node{ def pos: Position }
   case class Argument(pos: Position, name: String, typeRef: TypeNode) extends Node
   case class CompilationUnit(
-    pos: Position, sourceFile: String = "<generated>", module: ModuleDeclaration,
-    imports: ImportClause, toplevels: List[Toplevel]) extends Node
+    pos: Position, sourceFile: Option[String], module: Option[ModuleDeclaration],
+    imports: Option[ImportClause], toplevels: List[Toplevel]) extends Node
   case class ModuleDeclaration(pos: Position, name: String) extends Node
   case class ImportClause(pos: Position, mapping: List[(String, String)]) extends Node
   abstract sealed class Toplevel extends Node

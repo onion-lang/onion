a53c75c5a273e3a70a9346a4fe1f4e0c2e121171
kmizu <mizukota@gmail.com>
2010-12-05 10:49:10 +0900

    Add TypeChecker object to type check expressions.

---
+++ src.scala/onion/compiler/TypeChecker.scala
@@ -0,0 +1,14 @@
+package onion.compiler
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Mizushima
+ * Date: 2010/12/05
+ * Time: 10:46:42
+ * To change this template use File | Settings | File Templates.
+ */
+
+object TypeChecker {
+  def typeCheck(unit: AST.CompilationUnit) {
+  }
+}

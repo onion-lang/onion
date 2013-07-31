38e417d14cb72cddd0cfbf18a020c3e42a11ddc3
Kota Mizushima <mizukota@gmail.com>
2013-08-01 02:52:40 +0900

    Fix miss spelling

--- src/main/scala/onion/compiler/UnOrderedTable.scala
+++ src/main/scala/onion/compiler/UnOrderedTable.scala
@@ -3,10 +3,6 @@ package onion.compiler
 import java.util.HashMap
 
 /**
- * Created by IntelliJ IDEA.
- * User: Mizushima
- * Date: 2010/12/07
- * Time: 2:47:03
- * To change this template use File | Settings | File Templates.
+ * @author Kota Mizushima
  */
-class UnOrderedTable[E <: Named] extends AbstractTable[E](new HashMap[String, E])
+class UnorderedTable[E <: Named] extends AbstractTable[E](new HashMap[String, E])

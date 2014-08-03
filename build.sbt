de66f9ab59f8ffba1f39382b6305024811a7f743
Kota Mizushima <mizukota@gmail.com>
2014-08-04 02:10:02 +0900

    Split Build.scala into build.sbt and Build.scala

--- build.sbt
+++ build.sbt
@@ -1,1 +1,9 @@
+name := "onion"
+
+organization := "org.onion_lang"
+
+version := "0.1-SNAPSHOT"
+
+scalaVersion := "2.11.3"
+
 testOptions in Test += Tests.Argument("-u", "target/scalatest-reports")
--- project/Build.scala
+++ project/Build.scala
@@ -58,10 +58,6 @@ object Build extends Build {
   }
 
   lazy val onionSettigns = Defaults.defaultSettings ++ Seq(
-    name := "onion",
-    organization := "org.onion_lang",
-    version := "1.0-SNAPSHOT",
-    scalaVersion := "2.11.1",
     unmanagedSourceDirectories in Compile <<= (Seq(javaSource in Compile) ++ Seq(scalaSource in Compile) ++ Seq(sourceManaged in Compile)).join,
     scalacOptions ++= Seq("-encoding", "utf8", "-unchecked", "-deprecation", "-feature", "-language:implicitConversions", "-language:existentials"),
     javacOptions ++= Seq("-sourcepath", "src.lib", "-source", "1.5"),

9e8f3bc926e1e84564261d63e78c91cfa7abe9ff
Kota Mizushima <mizukota@gmail.com>
2016-11-06 09:08:18 +0900

    Remove useless class UnOrderedTable

--- src/main/scala/onion/compiler/UnOrderedTable.scala
+++
@@ -1,8 +0,0 @@
-package onion.compiler
-
-import scala.collection.mutable
-
-/**
- * @author Kota Mizushima
- */
-class UnOrderedTable[E <: Named] extends AbstractTable[E](new mutable.HashMap[String, E])

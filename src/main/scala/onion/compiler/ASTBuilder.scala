9d20621b633fa25129a40f6013df6cdb8499ca8b
Kota Mizushima <kmizu.main@gmail.com>
2025-05-26 23:37:08 +0900

    Refactor parser

---
+++ src/main/scala/onion/compiler/ASTBuilder.scala
@@ -0,0 +1,284 @@
+package onion.compiler
+
+import scala.collection.immutable.List
+
+trait ASTBuilder {
+  def createCompilationUnit(
+    location: Location,
+    sourceFile: String,
+    module: AST.ModuleDeclaration,
+    imports: AST.ImportClause,
+    toplevels: List[AST.Toplevel]
+  ): AST.CompilationUnit
+
+  def createModuleDeclaration(location: Location, name: String): AST.ModuleDeclaration
+
+  def createImportClause(location: Location, mapping: List[(String, String)]): AST.ImportClause
+
+  def createClassDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    superClass: AST.TypeNode,
+    interfaces: List[AST.TypeNode],
+    defaultSection: Option[AST.AccessSection],
+    sections: List[AST.AccessSection]
+  ): AST.ClassDeclaration
+
+  def createInterfaceDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    superTypes: List[AST.TypeNode],
+    methods: List[AST.MethodDeclaration]
+  ): AST.InterfaceDeclaration
+
+  def createRecordDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    args: List[AST.Argument]
+  ): AST.RecordDeclaration
+
+  def createMethodDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    args: List[AST.Argument],
+    returnType: AST.TypeNode,
+    body: AST.BlockExpression
+  ): AST.MethodDeclaration
+
+  def createConstructorDeclaration(
+    location: Location,
+    modifiers: Int,
+    args: List[AST.Argument],
+    params: List[AST.Expression],
+    body: AST.BlockExpression
+  ): AST.ConstructorDeclaration
+
+  def createFieldDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    typeRef: AST.TypeNode,
+    init: AST.Expression
+  ): AST.FieldDeclaration
+
+  def createBlockExpression(
+    location: Location,
+    statements: List[AST.CompoundExpression]
+  ): AST.BlockExpression
+
+  def createBinaryExpression(
+    location: Location,
+    operator: String,
+    lhs: AST.Expression,
+    rhs: AST.Expression
+  ): AST.Expression
+
+  def createUnaryExpression(
+    location: Location,
+    operator: String,
+    operand: AST.Expression
+  ): AST.Expression
+
+  def createLiteral(location: Location, value: Any): AST.Expression
+
+  def createIdentifier(location: Location, name: String): AST.Id
+
+  def createTypeNode(
+    location: Location,
+    desc: AST.TypeDescriptor,
+    isRelaxed: Boolean
+  ): AST.TypeNode
+
+  def createArgument(
+    location: Location,
+    name: String,
+    typeRef: AST.TypeNode
+  ): AST.Argument
+
+  def createAccessSection(
+    location: Location,
+    modifiers: Int,
+    members: List[AST.MemberDeclaration]
+  ): AST.AccessSection
+}
+
+class DefaultASTBuilder extends ASTBuilder {
+  def createCompilationUnit(
+    location: Location,
+    sourceFile: String,
+    module: AST.ModuleDeclaration,
+    imports: AST.ImportClause,
+    toplevels: List[AST.Toplevel]
+  ): AST.CompilationUnit = {
+    AST.CompilationUnit(location, sourceFile, module, imports, toplevels)
+  }
+
+  def createModuleDeclaration(location: Location, name: String): AST.ModuleDeclaration = {
+    AST.ModuleDeclaration(location, name)
+  }
+
+  def createImportClause(location: Location, mapping: List[(String, String)]): AST.ImportClause = {
+    AST.ImportClause(location, mapping)
+  }
+
+  def createClassDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    superClass: AST.TypeNode,
+    interfaces: List[AST.TypeNode],
+    defaultSection: Option[AST.AccessSection],
+    sections: List[AST.AccessSection]
+  ): AST.ClassDeclaration = {
+    AST.ClassDeclaration(location, modifiers, name, superClass, interfaces, defaultSection, sections)
+  }
+
+  def createInterfaceDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    superTypes: List[AST.TypeNode],
+    methods: List[AST.MethodDeclaration]
+  ): AST.InterfaceDeclaration = {
+    AST.InterfaceDeclaration(location, modifiers, name, superTypes, methods)
+  }
+
+  def createRecordDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    args: List[AST.Argument]
+  ): AST.RecordDeclaration = {
+    AST.RecordDeclaration(location, modifiers, name, args)
+  }
+
+  def createMethodDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    args: List[AST.Argument],
+    returnType: AST.TypeNode,
+    body: AST.BlockExpression
+  ): AST.MethodDeclaration = {
+    AST.MethodDeclaration(location, modifiers, name, args, returnType, body)
+  }
+
+  def createConstructorDeclaration(
+    location: Location,
+    modifiers: Int,
+    args: List[AST.Argument],
+    params: List[AST.Expression],
+    body: AST.BlockExpression
+  ): AST.ConstructorDeclaration = {
+    AST.ConstructorDeclaration(location, modifiers, args, params, body)
+  }
+
+  def createFieldDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    typeRef: AST.TypeNode,
+    init: AST.Expression
+  ): AST.FieldDeclaration = {
+    AST.FieldDeclaration(location, modifiers, name, typeRef, init)
+  }
+
+  def createBlockExpression(
+    location: Location,
+    statements: List[AST.CompoundExpression]
+  ): AST.BlockExpression = {
+    AST.BlockExpression(location, statements)
+  }
+
+  def createBinaryExpression(
+    location: Location,
+    operator: String,
+    lhs: AST.Expression,
+    rhs: AST.Expression
+  ): AST.Expression = {
+    operator match {
+      case "+" => AST.Addition(location, lhs, rhs)
+      case "-" => AST.Subtraction(location, lhs, rhs)
+      case "*" => AST.Multiplication(location, lhs, rhs)
+      case "/" => AST.Division(location, lhs, rhs)
+      case "%" => AST.Modulo(location, lhs, rhs)
+      case "==" => AST.Equal(location, lhs, rhs)
+      case "!=" => AST.NotEqual(location, lhs, rhs)
+      case "<" => AST.LessThan(location, lhs, rhs)
+      case "<=" => AST.LessOrEqual(location, lhs, rhs)
+      case ">" => AST.GreaterThan(location, lhs, rhs)
+      case ">=" => AST.GreaterOrEqual(location, lhs, rhs)
+      case "&&" => AST.LogicalAnd(location, lhs, rhs)
+      case "||" => AST.LogicalOr(location, lhs, rhs)
+      case "=" => AST.Assignment(location, lhs, rhs)
+      case "+=" => AST.AdditionAssignment(location, lhs, rhs)
+      case "-=" => AST.SubtractionAssignment(location, lhs, rhs)
+      case "*=" => AST.MultiplicationAssignment(location, lhs, rhs)
+      case "/=" => AST.DivisionAssignment(location, lhs, rhs)
+      case "%=" => AST.ModuloAssignment(location, lhs, rhs)
+      case ":?" => AST.Elvis(location, lhs, rhs)
+      case _ => throw new IllegalArgumentException(s"Unknown binary operator: $operator")
+    }
+  }
+
+  def createUnaryExpression(
+    location: Location,
+    operator: String,
+    operand: AST.Expression
+  ): AST.Expression = {
+    operator match {
+      case "+" => AST.Posit(location, operand)
+      case "-" => AST.Negate(location, operand)
+      case "!" => AST.Not(location, operand)
+      case "++" => AST.PostIncrement(location, operand)  // Note: Parser needs to handle pre/post
+      case "--" => AST.PostDecrement(location, operand)  // Note: Parser needs to handle pre/post
+      case _ => throw new IllegalArgumentException(s"Unknown unary operator: $operator")
+    }
+  }
+
+  def createLiteral(location: Location, value: Any): AST.Expression = {
+    value match {
+      case b: Boolean => AST.BooleanLiteral(location, b)
+      case c: Char => AST.CharacterLiteral(location, c)
+      case i: Int => AST.IntegerLiteral(location, i)
+      case l: Long => AST.LongLiteral(location, l)
+      case f: Float => AST.FloatLiteral(location, f)
+      case d: Double => AST.DoubleLiteral(location, d)
+      case s: String => AST.StringLiteral(location, s)
+      case null => AST.NullLiteral(location)
+      case _ => throw new IllegalArgumentException(s"Unknown literal type: ${value.getClass}")
+    }
+  }
+
+  def createIdentifier(location: Location, name: String): AST.Id = {
+    AST.Id(location, name)
+  }
+
+  def createTypeNode(
+    location: Location,
+    desc: AST.TypeDescriptor,
+    isRelaxed: Boolean
+  ): AST.TypeNode = {
+    AST.TypeNode(location, desc, isRelaxed)
+  }
+
+  def createArgument(
+    location: Location,
+    name: String,
+    typeRef: AST.TypeNode
+  ): AST.Argument = {
+    AST.Argument(location, name, typeRef)
+  }
+
+  def createAccessSection(
+    location: Location,
+    modifiers: Int,
+    members: List[AST.MemberDeclaration]
+  ): AST.AccessSection = {
+    AST.AccessSection(location, modifiers, members)
+  }
+}
---
+++ src/test/scala/onion/compiler/TestASTBuilder.scala
@@ -0,0 +1,193 @@
+package onion.compiler
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.diagrams.Diagrams
+import scala.collection.immutable.List
+
+/**
+ * Test implementation of ASTBuilder that demonstrates how the builder pattern
+ * allows for custom AST construction behavior without modifying the parser.
+ */
+class TestASTBuilder extends DefaultASTBuilder {
+  
+  // Track all method declarations for analysis
+  var methodCount = 0
+  var methodNames = List.empty[String]
+  
+  override def createMethodDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    args: List[AST.Argument],
+    returnType: AST.TypeNode,
+    body: AST.BlockExpression
+  ): AST.MethodDeclaration = {
+    // Custom behavior: track method statistics
+    methodCount += 1
+    methodNames = methodNames :+ name
+    
+    // Could also perform transformations here, e.g.:
+    // - Add logging to method bodies
+    // - Inject security checks
+    // - Optimize certain patterns
+    
+    super.createMethodDeclaration(location, modifiers, name, args, returnType, body)
+  }
+  
+  // Example: Automatically add toString methods to records
+  override def createRecordDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    args: List[AST.Argument]
+  ): AST.RecordDeclaration = {
+    // Could inject additional methods here
+    super.createRecordDeclaration(location, modifiers, name, args)
+  }
+}
+
+/**
+ * Logging AST builder that prints AST construction for debugging
+ */
+class LoggingASTBuilder extends DefaultASTBuilder {
+  
+  override def createClassDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    superClass: AST.TypeNode,
+    interfaces: List[AST.TypeNode],
+    defaultSection: Option[AST.AccessSection],
+    sections: List[AST.AccessSection]
+  ): AST.ClassDeclaration = {
+    println(s"Creating class: $name at $location")
+    super.createClassDeclaration(location, modifiers, name, superClass, interfaces, defaultSection, sections)
+  }
+  
+  override def createMethodDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    args: List[AST.Argument],
+    returnType: AST.TypeNode,
+    body: AST.BlockExpression
+  ): AST.MethodDeclaration = {
+    println(s"Creating method: $name with ${args.length} arguments at $location")
+    super.createMethodDeclaration(location, modifiers, name, args, returnType, body)
+  }
+}
+
+/**
+ * Validating AST builder that enforces coding standards
+ */
+class ValidatingASTBuilder extends DefaultASTBuilder {
+  
+  override def createMethodDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    args: List[AST.Argument],
+    returnType: AST.TypeNode,
+    body: AST.BlockExpression
+  ): AST.MethodDeclaration = {
+    // Enforce naming conventions
+    if (!name.matches("[a-z][a-zA-Z0-9]*")) {
+      throw new IllegalArgumentException(s"Method name '$name' does not follow camelCase convention at $location")
+    }
+    
+    // Enforce parameter limits
+    if (args.length > 10) {
+      throw new IllegalArgumentException(s"Method '$name' has too many parameters (${args.length}) at $location")
+    }
+    
+    super.createMethodDeclaration(location, modifiers, name, args, returnType, body)
+  }
+  
+  override def createClassDeclaration(
+    location: Location,
+    modifiers: Int,
+    name: String,
+    superClass: AST.TypeNode,
+    interfaces: List[AST.TypeNode],
+    defaultSection: Option[AST.AccessSection],
+    sections: List[AST.AccessSection]
+  ): AST.ClassDeclaration = {
+    // Enforce naming conventions
+    if (!name.matches("[A-Z][a-zA-Z0-9]*")) {
+      throw new IllegalArgumentException(s"Class name '$name' does not follow PascalCase convention at $location")
+    }
+    
+    super.createClassDeclaration(location, modifiers, name, superClass, interfaces, defaultSection, sections)
+  }
+}
+
+class ASTBuilderSpec extends AnyFunSpec with Diagrams {
+  describe("ASTBuilder pattern") {
+    it("allows custom AST construction behavior") {
+      val testBuilder = new TestASTBuilder()
+      val location = new Location(1, 1)
+      
+      // Create some methods
+      testBuilder.createMethodDeclaration(
+        location, 
+        AST.M_PUBLIC, 
+        "doSomething", 
+        List.empty, 
+        null, 
+        AST.BlockExpression(location, List.empty)
+      )
+      
+      testBuilder.createMethodDeclaration(
+        location, 
+        AST.M_PRIVATE, 
+        "helper", 
+        List.empty, 
+        null, 
+        AST.BlockExpression(location, List.empty)
+      )
+      
+      assert(testBuilder.methodCount == 2)
+      assert(testBuilder.methodNames == List("doSomething", "helper"))
+    }
+    
+    it("supports validation during AST construction") {
+      val validatingBuilder = new ValidatingASTBuilder()
+      val location = new Location(1, 1)
+      
+      // Valid method name
+      validatingBuilder.createMethodDeclaration(
+        location,
+        AST.M_PUBLIC,
+        "validMethodName",
+        List.empty,
+        null,
+        AST.BlockExpression(location, List.empty)
+      )
+      
+      // Invalid method name should throw
+      intercept[IllegalArgumentException] {
+        validatingBuilder.createMethodDeclaration(
+          location,
+          AST.M_PUBLIC,
+          "InvalidMethodName", // Starts with capital
+          List.empty,
+          null,
+          AST.BlockExpression(location, List.empty)
+        )
+      }
+      
+      // Too many parameters should throw
+      val tooManyArgs = (1 to 11).map(i => AST.Argument(location, s"arg$i", null)).toList
+      intercept[IllegalArgumentException] {
+        validatingBuilder.createMethodDeclaration(
+          location,
+          AST.M_PUBLIC,
+          "methodWithTooManyArgs",
+          tooManyArgs,
+          null,
+          AST.BlockExpression(location, List.empty)
+        )
+      }
+    }
+  }
+}

19126bfe285b76de0846188f9718c6b07dc9fc67
Kota Mizushima <kmizu.main@gmail.com>
2026-07-24 23:01:19 +0900

    Add unified Onion command routing

---
+++ src/main/scala/onion/tools/project/ProjectCommands.scala
@@ -0,0 +1,36 @@
+package onion.tools.project
+
+import java.io.PrintStream
+import java.nio.file.Path
+
+class ProjectCommands:
+  def create(name: String, cwd: Path, out: PrintStream, err: PrintStream): Int =
+    ProjectScaffolder.create(cwd, name) match
+      case Right(paths) =>
+        out.println(s"Created ${paths.root}")
+        0
+      case Left(error) =>
+        err.println(s"error: ${error.message}")
+        1
+
+  def build(cwd: Path, verbose: Boolean, out: PrintStream, err: PrintStream): Int =
+    notImplemented("build", err)
+
+  def run(
+    cwd: Path,
+    verbose: Boolean,
+    args: Array[String],
+    out: PrintStream,
+    err: PrintStream
+  ): Int =
+    notImplemented("run", err)
+
+  def test(cwd: Path, verbose: Boolean, out: PrintStream, err: PrintStream): Int =
+    notImplemented("test", err)
+
+  def clean(cwd: Path, out: PrintStream, err: PrintStream): Int =
+    notImplemented("clean", err)
+
+  private def notImplemented(command: String, err: PrintStream): Int =
+    err.println(s"error: onion $command is not implemented yet")
+    1
